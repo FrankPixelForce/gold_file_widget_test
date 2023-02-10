c9ac80eb6588326fbd98b7f531f3e297589cdb80
Frank Zhang <Frank.Zhang@pixelforce.com.au>
2023-02-10 17:22:33 +1030

    third push

--- test/widget/my_gold_test.dart
+++ test/widget/my_gold_test.dart
@@ -21,26 +21,77 @@ void main() {
       expect(find.text('1'), findsOneWidget);
       await screenMatchesGolden(tester, 'clicked_1_time_view');
     });
+    ///  官方文档  例子🌰
+    ///  官方文档  例子🌰
+    ///  官方文档  例子🌰
+    ///  第一列是 phone手机，   分别是： 没有点击，点击一次，点击5次
+    testGoldens('DeviceBuilder - multiple scenarios - with onCreate',
+            (tester) async {
+          final builder = DeviceBuilder()
+            ..overrideDevicesForAllScenarios(devices: [
+              Device.phone,
+              Device.iphone11,
+              Device.tabletPortrait,
+              Device.tabletLandscape,
+            ])
+            ..addScenario(
+              widget: MyHomePage(  title: '官方文档  例子🌰',),
+              name: 'default page',
+            )
+            ..addScenario(
+              widget: MyHomePage(  title: '官方文档  例子🌰',),
+              name: 'tap once',
+              onCreate: (scenarioWidgetKey) async {
+                final finder = find.descendant(
+                  of: find.byKey(scenarioWidgetKey),
+                  matching: find.byIcon(Icons.add),
+                );
+                expect(finder, findsOneWidget);
+                await tester.tap(finder);
+              },
+            )
+            ..addScenario(
+              widget: MyHomePage(  title: '官方文档  例子🌰',),
+              name: 'tap five times',
+              onCreate: (scenarioWidgetKey) async {
+                final finder = find.descendant(
+                  of: find.byKey(scenarioWidgetKey),
+                  matching: find.byIcon(Icons.add),
+                );
+                expect(finder, findsOneWidget);
+
+                await tester.tap(finder);
+                await tester.tap(finder);
+                await tester.tap(finder);
+                await tester.tap(finder);
+                await tester.tap(finder);
+              },
+            );
+
+          await tester.pumpDeviceBuilder(builder);
+
+          await screenMatchesGolden(tester, 'flutter_demo_page_multiple_scenarios');
+        });
   });
 
   ///
   group('match Golden file', () {
-    testWidgets('match Golden file', (widgetTester) async {
-      await widgetTester.pumpWidgetBuilder(const MyApp());
-      await expectLater(
-        find.byType(MyApp),
-        matchesGoldenFile('goldens/initial_view.png'),
-      );
-
-      ///
-      // Tap the '+' icon and trigger a frame.
-      await widgetTester.tap(find.byIcon(Icons.add));
-      await widgetTester.pump();
-      await expectLater(
-        find.byType(MyApp),
-        matchesGoldenFile('goldens/clicked_1_time_view.png'),
-      );
-    });
+    // testWidgets('match Golden file', (widgetTester) async {
+    //   await widgetTester.pumpWidgetBuilder(const MyApp());
+    //   await expectLater(
+    //     find.byType(MyApp),
+    //     matchesGoldenFile('goldens/initial_view.png'),
+    //   );
+    //
+    //   ///
+    //   // Tap the '+' icon and trigger a frame.
+    //   await widgetTester.tap(find.byIcon(Icons.add));
+    //   await widgetTester.pump();
+    //   await expectLater(
+    //     find.byType(MyApp),
+    //     matchesGoldenFile('goldens/clicked_1_time_view.png'),
+    //   );
+    // });
   });
   /// second push
 }
